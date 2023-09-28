e971de374717a1100fb1971e4d5dc65a59d54e44
Kania Purnarahayu <114629927+Kankania@users.noreply.github.com>
2023-09-28 17:27:30 +0700

    Add files via upload

---
+++ tugassesisatu.js
@@ -0,0 +1,12 @@
+//Program.js
+
+//Identitas
+const nama = "Kania Purnarhayu";
+const nim = "20220040099";
+const tahunLahir = 2004;
+const tahunSekarang = 2023;
+const umur = tahunSekarang - tahunLahir;
+
+console.log(`NAMA : ${nama}`);
+console.log(`NIM : ${nim}`);
+console.log(`UMUR : ${umur}`);
