beb8fa68a5f9c39f5bec00cb9625a99ee36ca6ba
Kania Purnarahayu <114629927+Kankania@users.noreply.github.com>
2023-10-04 18:22:39 +0700

    Add files via upload

---
+++ tugas2.js
@@ -0,0 +1,10 @@
+//Identitas
+var firstName = "Kania";
+var lastName = "Purnarahayu";
+var age = 19;
+var isMarried = false;
+
+console.log(`FIRST NAME : ${firstName}`);
+console.log(`LAST NAME : ${lastName}`);
+console.log(`AGE : ${age}`);
+console.log(`IS MARRIED : ${isMarried}`);
