a2e288fce5a9a9d63a9f163bf5c4ee5a9338d5c2
Kania Purnarahayu <114629927+Kankania@users.noreply.github.com>
2023-10-19 18:12:16 +0700

    Add files via upload

---
+++ TUGAS3.js
@@ -0,0 +1,40 @@
+console.log("Nama : Kania Purnarahayu")
+console.log("NIM : 20220040099")
+console.log("Kelas : TI22A")
+console.log("--------------------------------------------------------------------------------")
+
+//Variabel dengan tipe objek
+let restaurant = {
+    name : "Resto A",
+    city : "Sukabumi",
+    favoriteDrink : "Apple Juice",
+    favoriteFood : "Sausage",
+    isVegan : false
+};
+
+//Variabel dengan tipe array
+let restaurants = [
+    {
+    name : "Resto A",
+    city : "Sukabumi",
+    favoriteDrink : "Apple Juice",
+    favoriteFood : "Sausage",
+    isVegan : false
+    },
+{
+    name : "Resto B",
+    city : "Bandung",
+    favoriteDrink : "Iced Tea",
+    favoriteFood : "Burger",
+    isVegan : true
+    },
+    {   
+    name : "Resto C",
+    city : "Jakarta",
+    favoriteDrink: "Orange Juice",
+    favoriteFood : "Pizza",
+    isVegan : true  
+    }
+];
+console.log(restaurant);
+console.log(restaurants);
